6c5bb8c8123fe3103cd381ab5c0593099c532aaa
jaydensipe <jayden.sipe@gmail.com>
2022-01-23 05:27:40 -0500

    Updated Angular app changes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,28 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit  {
+export class AppComponent implements OnInit {
   title = 'Udemus Dateus';
   users: any;
 
-  constructor(private http: HttpClient) {
+  constructor(private accountService: AccountService) {
 
   }
 
   ngOnInit() {
-    this.getUsers();
+    this.setCurrentUser();
   }
 
-  getUsers(){
-    this.http.get("https://localhost:5001/api/users").subscribe(res => {
-      this.users = res;
-    }, error => {
-      console.log(error);
-    })
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
 }
